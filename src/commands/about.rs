fde41f2017ffd2eb7ab5cbb3c361cff6f0580c5e
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-06-27 19:07:43 +0900

    feat about

---
+++ src/commands/about.rs
@@ -0,0 +1,20 @@
+use crate::{Context, Exception};
+use poise::serenity_prelude as serenity;
+use ::serenity::all::CreateEmbedAuthor;
+
+#[poise::command(slash_command)]
+pub async fn about(ctx: Context<'_>) -> Result<(), Exception> {
+
+    let msg = {
+        let embed = serenity::CreateEmbed::default()
+        .author(CreateEmbedAuthor::new("About"))
+        .description("FF14の各種情報を提供します。")
+        .color(0x0073d1);
+
+        poise::CreateReply::default().embed(embed)
+    };
+
+    ctx.send(msg).await?;
+
+    Ok(())
+}
