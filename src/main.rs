4bbf6c72bea332d5e2c38c2853441d83b50dc2cd
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-07-22 14:48:52 +0900

    impl database and market

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,21 @@ mod commands {
     pub mod about;
     pub mod news;
     pub mod market;
-    pub mod weather;
+    // pub mod weather;
 }
 
 mod utils {
     pub mod paginate;
+    pub mod market;
 }
 
 type Exception = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Exception>;
 
-pub struct Data {}
+pub struct Data {
+    // loaded_data: Vec<market::Item>,
+    db: sqlx::SqlitePool,
+}
 
 #[tokio::main]
 async fn main() {
@@ -39,14 +43,34 @@ async fn main() {
                 // commands::weather::weather()
                 commands::market::market(),
                 ],
+            on_error: |error| {
+                    Box::pin(async move {
+                        println!("what the hell");
+                        match error {
+                            poise::FrameworkError::ArgumentParse { error, .. } => {
+                                if let Some(error) = error.downcast_ref::<serenity::RoleParseError>() {
+                                    println!("Found a RoleParseError: {:?}", error);
+                                } else {
+                                    println!("Not a RoleParseError :(");
+                                }
+                            }
+                            other => poise::builtins::on_error(other).await.unwrap(),
+                        }
+                    })
+                },
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                poise::builtins::register_in_guild(ctx, &framework.options().commands, serenity::GuildId::new(1140679501852966994)).await?;
-                poise::builtins::register_in_guild(ctx, &framework.options().commands, serenity::GuildId::new(1169613731269984376)).await?;
-                Ok(Data {})
+                // poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                // poise::builtins::register_in_guild(ctx, &framework.options().commands, serenity::GuildId::new(1140679501852966994)).await?; //一瞬間が近いよ
+                poise::builtins::register_in_guild(ctx, &framework.options().commands, serenity::GuildId::new(1169613731269984376)).await?; // sasagusa folder
+
+                let conn = sqlx::sqlite::SqlitePool::connect("sqlite://sqlite.db").await.unwrap();
+                Ok(Data {
+                    // loaded_data: load_items("src/resources/items.json"),
+                    db: conn,
+                })
             })
         })
         .build();
