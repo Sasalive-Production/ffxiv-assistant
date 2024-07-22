4c79cdf0fb5f79acbf064d5d84b57d2c029441e5
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-07-22 14:13:16 +0900

    delete deadcode

--- src/commands/weather.rs
+++
@@ -1,9 +0,0 @@
-use crate::{utils::{self, weather::calculate_weather}, Context, Exception};
-use poise::serenity_prelude as serenity;
-use crate::utils::weather;
-
-#[poise::command(slash_command)]
-pub async fn weather(ctx: Context<'_>) -> Result<(), Exception> {
-    // write your code here
-    Ok(())
-}
